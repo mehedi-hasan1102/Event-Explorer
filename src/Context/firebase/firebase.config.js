cb4f37bf1c53e04ff236a448549a579d51d3568a
Mehedi Hasan <dev.hasan10@gmail.com>
2025-06-30 17:51:46 +0600

    Update firebase.config.js

--- src/Context/firebase/firebase.config.js
+++ src/Context/firebase/firebase.config.js
@@ -1,23 +1,4 @@
 
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCtneB_I-qar2_Q_qTGDgBpdRUkQ0Kp4Rs",
-//   authDomain: "event-explorer-a09.firebaseapp.com",
-//   projectId: "event-explorer-a09",
-//   storageBucket: "event-explorer-a09.appspot.com", 
-//   messagingSenderId: "966811565561",
-//   appId: "1:966811565561:web:f1fb2f63619f7a7f77d128"
-// };
-
-
-// const app = initializeApp(firebaseConfig);
-
-
-// export const auth = getAuth(app);
-// export default app;
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
